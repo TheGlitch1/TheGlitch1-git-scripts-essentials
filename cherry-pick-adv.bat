f511db6c49b5a591cc6da66e49f9bd6707bca4d8
Yassine NATIJ <yassine.natij@atos.net>
2023-02-25 20:59:33 +0100

    Fix pagination issue forward Changed the skip and max count

--- cherry-pick-adv.bat
+++ cherry-pick-adv.bat
@@ -1,6 +1,5 @@
 @echo off
 
-
 @REM Store the initial path from where the script was exec
 set "init_path=%cd%"
 
@@ -23,36 +22,44 @@ echo.
 setlocal EnableDelayedExpansion
 :cherry_pick_loop
 set count=0
-@REM TODO: message array doest contain the fukk commit message with tokens=1,2
+set start=1
+set end=5
+
+@REM just in case i want to readjust paging --skip !start! --max-count=!end!
 for /f "tokens=1,*" %%a in ('git log --oneline %branch%') do (
     set /a count+=1
     set hash[!count!]=%%a
     set message[!count!]=%%b
-    if !count! equ 5 (
-        goto :break
-    )
 )
 
-:break
-set /a endcount=count
-
-:menu
 cls
-echo "The last 5 commits on the %branch% branch are: "
+echo "The commits on the %branch% branch are: "
 echo.
 
-for /l %%i in (1,1,!endcount!) do (
+:loop
+@REM !start!,1,!count!
+for /l %%i in (!start!,1,!end!) do (
     echo [%%i] !hash[%%i]! !message[%%i]!
 )
 
 echo.
-set /p selection="Enter the number of the commit to cherry-pick, or q to quit: "
+if !end! lss !count! (
+    set /p selection="Enter 'n' to see the next 5 commits or the number of the commit to cherry-pick, or q to quit: "
+) else (
+    set /p selection="Enter the number of the commit to cherry-pick, or q to quit: "
+)
+
 if /i !selection! equ q exit /b
+if /i !selection! equ n (
+    set /a start+=5
+    set /a end+=5
+    goto loop
+)
 
 if not defined hash[%selection%] (
     echo Invalid selection. Press any key to try again...
     pause >nul
-    goto :menu
+    goto :cherry_pick_loop
 )
 
 echo.
@@ -67,11 +74,8 @@ set /p choice=Do you want to cherry-pick another commit? (Y/N)
 if /i "%choice%"=="Y" (
     set /p same_branch=Is it from the same branch? (Y/N)
     if /i "%same_branch%"=="Y" (
-        @REM git log %branch% --oneline -n 5 --skip 5
         goto cherry_pick_loop
     ) else (
-        @REM set /p branch=Enter the new branch name to cherry-pick from:
-        @REM git log %branch% --oneline -n 5
         goto search_branch
     )
 )
@@ -81,3 +85,7 @@ echo "Don't forget to put a star on the repo. See you soon!"
 echo.
 echo Press any key to exit...
 pause >nul
+
+:end_loop
+endlocal
+cd /d "%init_path%"
